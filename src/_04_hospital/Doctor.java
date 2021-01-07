63c36cd6a78e6f166ff2ee7e89d251d8be16e0e2
jbear18 <shehu01@hotmail.com>
2021-01-06 19:58:19 -0800

    hospital ip

---
+++ src/_04_hospital/Doctor.java
@@ -0,0 +1,32 @@
+package _04_hospital;
+
+import java.util.List;
+
+public class Doctor {
+
+	public boolean performsSurgery() {
+
+		return false;
+	}
+
+	public boolean makesHouseCalls() {
+
+		return false;
+	}
+
+	public void assignPatient(Patient patient) throws DoctorException{
+	
+		
+	}
+
+	public List<Doctor> getPatients() {
+
+		return null;
+	}
+
+	public void doMedicine() {
+
+		
+	}
+
+}
---
+++ src/_04_hospital/Hospital.java
@@ -0,0 +1,31 @@
+package _04_hospital;
+
+import java.util.List;
+
+public class Hospital {
+
+	public void addDoctor(Doctor doctor) {
+
+		
+	}
+
+
+	public List<Doctor> getDoctors() {
+		return null;
+	}
+
+	public void addPatient(Patient patient) {
+		
+	}
+
+	public List<Doctor> getPatients() {
+
+		return null;
+	}
+
+	public void assignPatientsToDoctors() {
+	
+		
+	}
+
+}
